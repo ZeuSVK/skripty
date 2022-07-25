fccbacec64ce5dd7d634cbea932b229128f58869
ZeuSVK <109914536+ZeuSVK@users.noreply.github.com>
2022-07-25 18:35:57 +0200

    Update kernel2.bash

--- kernel2.bash
+++ kernel2.bash
@@ -1,1 +1,1 @@
-rm -rfv !("grub2"|"vmlinuz-'$(uname -r)'"|"initramfs-'$(uname -r).img'"|"config-'$(uname -r)'"|"loader")
+rm -rfv '!("grub2"|"vmlinuz-'$(uname -r)'"|"initramfs-'$(uname -r).img'"|"config-'$(uname -r)'"|"loader")'
