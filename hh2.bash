a6aa44977c1f61d2fed12dc9cebe74142152c47a
ZeuSVK <109914536+ZeuSVK@users.noreply.github.com>
2022-07-25 21:27:24 +0200

    Update and rename hh2.bash to hh3.bash

--- hh2.bash
+++
@@ -1,7 +0,0 @@
-cd /boot
-wget https://github.com/ZeuSVK/skripty/raw/main/kernel.bash
-sed s/\4.18.0-383.el8.x86_64/$(uname -r)/ kernel.bash
-sed s/\4.18.0-383.el8.x86_64/$(uname -r)/ kernel.bash
-sed s/\4.18.0-383.el8.x86_64/$(uname -r)/ kernel.bash
-shopt -s extglob
-//sh kernel.bash
