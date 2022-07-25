b158aa1b8a00e50fca850bf637bd940b2b3a18d6
ZeuSVK <109914536+ZeuSVK@users.noreply.github.com>
2022-07-25 21:26:05 +0200

    Update and rename hh1.bash to hh2.bash

--- hh2.bash
+++ hh2.bash
@@ -4,4 +4,4 @@ sed s/\4.18.0-383.el8.x86_64/$(uname -r)/ kernel.bash
 sed s/\4.18.0-383.el8.x86_64/$(uname -r)/ kernel.bash
 sed s/\4.18.0-383.el8.x86_64/$(uname -r)/ kernel.bash
 shopt -s extglob
-sh kernel.bash
+//sh kernel.bash
