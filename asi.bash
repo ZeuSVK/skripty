e1c275179680c10f84715bb2cc4d6c4d499d4889
ZeuSVK <109914536+ZeuSVK@users.noreply.github.com>
2022-07-26 19:52:25 +0200

    Update asi.bash

--- asi.bash
+++ asi.bash
@@ -1,16 +1,10 @@
-rm -rfv /usr/lib64/libmozjs-60.so.0.0.0
-find /usr/lib64/ -type f -name "libpipe*" -print -delete
-rm -rfv /usr/lib64/gnome-settings-daemon-3.0
-find /usr/lib64/ -type f -name "libgtk*" -print -delete
-find /usr/lib64/ -type f -name "*bluetooth*" -print -delete
-find /usr/lib64/ -type f -name "*gnome*" -print -delete
-find /usr/lib64/ -type f -name "*polkit*" -print -delete
-find /usr/lib64/ -type f -name "*samba*" -print -delete
-find /usr/lib64/ -type f -name "*libcol*" -print -delete
-find /usr/lib64/ -type f -name "*jpg*" -print -delete
-find / -iname "*jpg*" -print -delete
-rm -rfv /usr/share/themes
-find / -name "*svg*" -print -delete
-find / -name "*icon*" -print -delete
-find / -name "*gtk*" -print -delete
-find / -name "*LLVM*" -print -delete
+find / -name "*libbluetoth*" -print -delete
+find / -name "*girepository*" -print -delete
+find / -name "*aspell*" -print -delete
+find / -name "*libbrotli*" -print -delete
+find / -name "*libdb*" -print -delete
+find / -name "*libedataserver*" -print -delete
+find / -name "*libfdisk*" -print -delete
+find / -name "*libgdk*" -print -delete
+find / -name "*libgnome*" -print -delete
+find / -name "*libldb*" -print -delete
