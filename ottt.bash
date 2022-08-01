c0e0f9f7a972f8c01fe3c1f55ee609940fc1e5ad
ZeuSVK <109914536+ZeuSVK@users.noreply.github.com>
2022-08-01 14:43:57 +0200

    Update ottt.bash

--- ottt.bash
+++ ottt.bash
@@ -1,1 +1,2 @@
+dnf install -y https://download.opensuse.org/source/distribution/leap/15.4/repo/oss/src/hunspell-1.6.2-3.8.1.src.rpm
 dnf install -y https://download-ib01.fedoraproject.org/pub/fedora/linux/releases/36/Everything/x86_64/os/Packages/o/otter-browser-1.0.03-1.fc36.x86_64.rpm
