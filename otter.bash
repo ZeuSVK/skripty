3cff98fe1c21ed300dc57ae095c4d82980592105
ZeuSVK <109914536+ZeuSVK@users.noreply.github.com>
2022-08-20 12:36:26 +0200

    Delete otter.bash

--- otter.bash
+++
@@ -1,4 +0,0 @@
-rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
-rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
-dnf install -y https://download.opensuse.org/source/distribution/leap/15.4/repo/oss/src/hunspell-1.6.2-3.8.1.src.rpm
-dnf install -y https://download-ib01.fedoraproject.org/pub/fedora/linux/releases/36/Everything/x86_64/os/Packages/o/otter-browser-1.0.03-1.fc36.x86_64.rpm
